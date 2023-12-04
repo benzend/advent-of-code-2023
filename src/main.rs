d9c28215d87edcae31f25579bc71e8d5be40a680
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-12-03 23:33:30 -0700

    feat: day 2 part 1 solve

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 #[cfg(test)]
 mod tests {
     use crate::advent;
-    use crate::{first_and_last_num, first_and_last_num_int_n_txt};
+    use crate::{first_and_last_num, first_and_last_num_int_n_txt, valid_game};
 
     #[test]
     fn test_advent_day_1_part_1() {
@@ -93,6 +93,96 @@ mod tests {
         }
 
         assert_eq!(sum, 11 + 38 + 14 + 42 + 14);
+    }
+
+    #[test]
+    fn test_advent_day_2_part_1() {
+        let data = advent::get_input(advent::AdventDay::new(2), None).unwrap();
+
+        let mut valid_ids = Vec::new();
+        for line in data.lines() {
+            let deets = line.split(":").enumerate().map(|(i, p)| {
+                if i == 0 {
+                    p.replace("Game ", "")
+                } else {
+                    p.to_string()
+                }
+            }).collect::<Vec<String>>();
+
+            let id = deets[0].to_string();
+            let game = deets[1].to_string();
+
+            // only 12 red cubes, 13 green cubes, and 14 blue cubes
+            let rules = crate::GameRules { red: 12, green: 13, blue: 14 };
+            if valid_game(crate::Game::new(&game), rules) {
+                valid_ids.push(id.to_string().parse::<usize>().unwrap());
+            }
+        }
+
+        let mut sum = 0;
+        for id in valid_ids.iter() {
+            sum += id;
+        }
+
+        assert_eq!(sum, 200);
+    }
+}
+
+fn valid_game(game: Game, rules: GameRules) -> bool {
+    game.max_red <= rules.red && game.max_green <= rules.green && game.max_blue <= rules.blue
+}
+
+struct GameRules {
+    pub red: usize,
+    pub green: usize,
+    pub blue: usize,
+}
+
+struct Game {
+    pub max_red: usize,
+    pub max_green: usize,
+    pub max_blue: usize
+}
+
+impl Game {
+    pub fn new(s: &str) -> Self {
+        let mut max_red = 0;
+        let mut max_green = 0;
+        let mut max_blue = 0;
+        let sets = s.split(";");
+        for set in sets {
+            let colors = set.split(",");
+
+            for color in colors {
+                let items = color.trim().split(" ").collect::<Vec<&str>>();
+                let (val, label) = (items[0], items[1]);
+                let val = val.parse::<usize>().unwrap();
+
+                match label {
+                    "red" => {
+                        if val > max_red {
+                            max_red = val;
+                        }
+                    }
+                    "green" => {
+                        if val > max_green {
+                            max_green = val;
+                        }
+                    }
+                    "blue" => {
+                        if val > max_blue {
+                            max_blue = val;
+                        }
+                    }
+                    v => panic!("what kind of color is this?? ({})", v)
+                }
+            }
+        }
+        Game {
+            max_red,
+            max_green,
+            max_blue
+        }
     }
 }
 
