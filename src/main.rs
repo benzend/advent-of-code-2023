49b12bd61bb230b3147b10cb72ef412e263fd2c2
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-12-05 07:40:01 -0700

    chore: solve day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -100,19 +100,27 @@ mod tests {
 
         let mut valid_ids = Vec::new();
         for line in data.lines() {
-            let deets = line.split(":").enumerate().map(|(i, p)| {
-                if i == 0 {
-                    p.replace("Game ", "")
-                } else {
-                    p.to_string()
-                }
-            }).collect::<Vec<String>>();
+            let deets = line
+                .split(":")
+                .enumerate()
+                .map(|(i, p)| {
+                    if i == 0 {
+                        p.replace("Game ", "")
+                    } else {
+                        p.to_string()
+                    }
+                })
+                .collect::<Vec<String>>();
 
             let id = deets[0].to_string();
             let game = deets[1].to_string();
 
             // only 12 red cubes, 13 green cubes, and 14 blue cubes
-            let rules = crate::GameRules { red: 12, green: 13, blue: 14 };
+            let rules = crate::GameRules {
+                red: 12,
+                green: 13,
+                blue: 14,
+            };
             if crate::valid_game(crate::Game::new(&game), rules) {
                 valid_ids.push(id.to_string().parse::<usize>().unwrap());
             }
@@ -123,7 +131,74 @@ mod tests {
             sum += id;
         }
 
+        assert_eq!(sum, 2076);
+    }
+
+    #[test]
+    fn test_advent_day_2_part_2() {
+        let data = advent::get_input(advent::AdventDay::new(2), None).unwrap();
+
+        let mut sum = 0;
+        for line in data.lines() {
+            let deets = line
+                .split(":")
+                .enumerate()
+                .map(|(i, p)| {
+                    if i == 0 {
+                        p.replace("Game ", "")
+                    } else {
+                        p.to_string()
+                    }
+                })
+                .collect::<Vec<String>>();
+
+            let game = deets[1].to_string();
+
+            let game = crate::Game::new(&game);
+            sum += game.max_red * game.max_green * game.max_blue;
+        }
+
         assert_eq!(sum, 200);
+    }
+
+    #[test]
+    fn test_advent_day_2_part_2_example() {
+        let data = vec![
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ];
+
+        let mut sum = 0;
+        for (i, line) in data.iter().enumerate() {
+            let deets = line
+                .split(":")
+                .enumerate()
+                .map(|(i, p)| {
+                    if i == 0 {
+                        p.replace("Game ", "")
+                    } else {
+                        p.to_string()
+                    }
+                })
+                .collect::<Vec<String>>();
+
+            let game = deets[1].to_string();
+
+            let game = crate::Game::new(&game);
+
+            if i == 0 {
+                assert_eq!(game.max_red, 4);
+                assert_eq!(game.max_green, 2);
+                assert_eq!(game.max_blue, 6);
+            }
+
+            sum += game.max_red * game.max_green * game.max_blue;
+        }
+
+        assert_eq!(sum, 2286);
     }
 }
 
@@ -140,7 +215,7 @@ struct GameRules {
 struct Game {
     pub max_red: usize,
     pub max_green: usize,
-    pub max_blue: usize
+    pub max_blue: usize,
 }
 
 impl Game {
@@ -148,6 +223,7 @@ impl Game {
         let mut max_red = 0;
         let mut max_green = 0;
         let mut max_blue = 0;
+
         let sets = s.split(";");
         for set in sets {
             let colors = set.split(",");
@@ -173,14 +249,14 @@ impl Game {
                             max_blue = val;
                         }
                     }
-                    v => panic!("what kind of color is this?? ({})", v)
+                    v => panic!("what kind of color is this?? ({})", v),
                 }
             }
         }
         Game {
             max_red,
             max_green,
-            max_blue
+            max_blue,
         }
     }
 }
